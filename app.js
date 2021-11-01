ce58736f110b56888970a5aebf112e1ac54a6d19
harlita <harlitad@gmail.com>
2021-11-01 21:42:52 +0700

    init app

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const indexRouter = require('./app/routes')
+
+app.use(indexRouter)
+
+app.listen(port, () => console.log(`Server run in ${port}`));
+
+module.exports = app
