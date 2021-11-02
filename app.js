8e0794146c05336a7d5d09a95a5ece46fc62f77a
harlita <harlitad@gmail.com>
2021-11-02 10:41:51 +0700

    add middleware json parser

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 const indexRouter = require('./app/routes')
 
+app.use(express.json())
 app.use(indexRouter)
 
 app.listen(port, () => console.log(`Server run in ${port}`));
