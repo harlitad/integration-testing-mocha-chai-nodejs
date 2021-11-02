38758910749b0298fc997641479f6d96a6538c65
harlita <harlitad@gmail.com>
2021-11-02 10:39:58 +0700

    rename users route file

--- app/routes/users.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const userController = require('./../controllers/user.controller')
-
-router.get('/', userController.getAllUser)
-
-module.exports = router
