10fc385bcb292cf97d8eee27986d63af22fb82aa
harlita <harlitad@gmail.com>
2021-11-02 10:43:38 +0700

    add service create and delete users

--- app/services/user.service.js
+++ app/services/user.service.js
@@ -1,4 +1,6 @@
-const users = require('./../data/users.json')
+let users = require('./../data/users.json')
+const { writeFile } = require('fs');
+const path = __dirname + '/../data/users.json';
 
 exports.findAll = () => {
     return users;
@@ -9,8 +11,27 @@ exports.findOneById = (id) => {
 }
 
 exports.create = (newUser) => {
-    newUser.id = String(Number(users.length) + 1);
+    newUser.id = String(users.length + 1);
     newUser.status = true;
-    users = users.push(newUser)
+    users.push(newUser)
+    writeFile(path, JSON.stringify(users, null, 4), (err) => {
+        if(err){
+            console.log('error when write the json')
+            return
+        }
+    })
     return newUser
+}
+
+exports.delete = (id) => {
+    if(users.find(user => user.id === id)) {
+        writeFile(path, JSON.stringify(users.filter(user => user.id !== id), null, 4), (err) => {
+            if(err){
+                console.log('error when write the json')
+                return
+            }
+        })
+        return true
+    }
+    return false
 }
