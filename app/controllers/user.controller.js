2ca48c617e4ff331079a339f93ba0742251db171
harlita <harlitad@gmail.com>
2021-11-02 10:42:34 +0700

    add create and delete users function

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,9 +1,22 @@
 const userService = require('./../services/user.service')
-const {baseResponsePagination} = require('./../utils/helper/response')
+const {baseResponsePagination, baseResponse} = require('./../utils/helper/response')
 
 exports.getAllUser = async (req, res) => {
     const users = userService.findAll()
     const limit = req.query.limit
     const page = req.query.page
     return res.json(baseResponsePagination({data:users, limit, page}))
+}
+
+exports.createUser = async (req, res) => {
+    const users = userService.create(req.body)
+    return res.status(201).json(baseResponse({data:users, message:'success create user'}))
+}
+
+exports.deleteUser = async (req, res) => {
+    const users = userService.delete(req.params.id)
+    if(users) {
+        return res.status(204).json()
+    }
+    return res.status(404).json(baseResponse({message:`user with the id ${req.params.id} not found!`}))
 }
